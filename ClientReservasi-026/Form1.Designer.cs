00ca82194de1f51c64fa1424ca4d6f637daa288e
Muhammad Risqullah <m.risqullah.ft19@mail.umy.ac.id>
2021-12-20 16:47:49 +0700

    Add project files.

---
+++ ClientReservasi-026/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace ClientReservasi_026
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dtPemesanan = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.tbNoTelp = new System.Windows.Forms.TextBox();
+            this.tbJumlah = new System.Windows.Forms.TextBox();
+            this.tbIDLok = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtPemesanan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(655, 42);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(75, 23);
+            this.btnSimpan.TabIndex = 0;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(655, 108);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(655, 162);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 23);
+            this.btnHapus.TabIndex = 2;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(655, 221);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // dtPemesanan
+            // 
+            this.dtPemesanan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtPemesanan.Location = new System.Drawing.Point(82, 266);
+            this.dtPemesanan.Name = "dtPemesanan";
+            this.dtPemesanan.RowHeadersWidth = 51;
+            this.dtPemesanan.RowTemplate.Height = 24;
+            this.dtPemesanan.Size = new System.Drawing.Size(544, 150);
+            this.dtPemesanan.TabIndex = 4;
+            this.dtPemesanan.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtPemesanan_CellClick);
+            this.dtPemesanan.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtPemesanan_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ID Reservasi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "No Telpon";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Jumlah Pemesanan";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "ID Lokasi";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(190, 37);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(405, 22);
+            this.tbID.TabIndex = 10;
+            // 
+            // tbNama
+            // 
+            this.tbNama.Location = new System.Drawing.Point(190, 86);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(405, 22);
+            this.tbNama.TabIndex = 11;
+            // 
+            // tbNoTelp
+            // 
+            this.tbNoTelp.Location = new System.Drawing.Point(190, 135);
+            this.tbNoTelp.Name = "tbNoTelp";
+            this.tbNoTelp.Size = new System.Drawing.Size(405, 22);
+            this.tbNoTelp.TabIndex = 12;
+            // 
+            // tbJumlah
+            // 
+            this.tbJumlah.Location = new System.Drawing.Point(190, 180);
+            this.tbJumlah.Name = "tbJumlah";
+            this.tbJumlah.Size = new System.Drawing.Size(405, 22);
+            this.tbJumlah.TabIndex = 13;
+            this.tbJumlah.TextChanged += new System.EventHandler(this.tbJumlah_TextChanged);
+            // 
+            // tbIDLok
+            // 
+            this.tbIDLok.Location = new System.Drawing.Point(190, 224);
+            this.tbIDLok.Name = "tbIDLok";
+            this.tbIDLok.Size = new System.Drawing.Size(405, 22);
+            this.tbIDLok.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbIDLok);
+            this.Controls.Add(this.tbJumlah);
+            this.Controls.Add(this.tbNoTelp);
+            this.Controls.Add(this.tbNama);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtPemesanan);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSimpan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtPemesanan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dtPemesanan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.TextBox tbNoTelp;
+        private System.Windows.Forms.TextBox tbJumlah;
+        private System.Windows.Forms.TextBox tbIDLok;
+    }
+}
+
---
+++ ClientReservasi-026/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientReservasi_026
+{
+    public partial class Form1 : Form
+    {
+        ServiceReference1.Service1Client service = new ServiceReference1.Service1Client();
+        public Form1()
+        {
+            
+            InitializeComponent();
+            TampilData();
+            btnUpdate.Enabled = false;
+            btnHapus.Enabled = false;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbJumlah_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            string IDPemesanan = tbID.Text;
+            string NamaCustomer = tbNama.Text;
+            string NoTelpon = tbNoTelp.Text;
+            string idLokasi = tbIDLok.Text;
+            int jumlahpemesanan = int.Parse(tbJumlah.Text);
+
+            var a = service.pemesanan(IDPemesanan, NamaCustomer, NoTelpon, jumlahpemesanan, idLokasi);
+            MessageBox.Show(a);
+            TampilData();
+            Clear();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string IDPemesanan = tbID.Text;
+            string NamaCustomer = tbNama.Text;
+            string NoTelpon = tbNoTelp.Text;
+
+            var a = service.editpemesanan(IDPemesanan, NamaCustomer, NoTelpon);
+            MessageBox.Show(a);
+            TampilData();
+            Clear();
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            string IDPemesanan = tbID.Text;
+            var a = service.deletePemesanan(IDPemesanan);
+            MessageBox.Show(a);
+            TampilData();
+            Clear();
+        }
+        public void TampilData()
+        {
+            var List = service.Pemesanan1();
+            dtPemesanan.DataSource = List;
+        }
+        
+        public void Clear()
+        {
+            tbID.Clear();
+            tbIDLok.Clear();
+            tbJumlah.Clear();
+            tbNama.Clear();
+            tbNoTelp.Clear();
+
+            tbJumlah.Enabled = true;
+            tbIDLok.Enabled = true;
+            btnSimpan.Enabled = true;
+            btnUpdate.Enabled = false;
+            btnHapus.Enabled = false;
+            tbID.Enabled = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void dtPemesanan_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dtPemesanan_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            tbID.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[0].Value);
+            tbNama.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[1].Value);
+            tbNoTelp.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[2].Value);
+            tbJumlah.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[3].Value);
+            tbIDLok.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[4].Value);
+
+            tbJumlah.Enabled = false;
+            tbIDLok.Enabled = false;
+
+            btnUpdate.Enabled = true;
+            btnHapus.Enabled = true;
+            btnSimpan.Enabled = false;
+            tbID.Enabled = false;
+        }
+    }
+}
